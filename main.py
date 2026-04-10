82dce8d9a5def7406711ec12b798fa6813c436a0
Priyanka Kansal <priyanka01kansal@gmail.com>
2026-04-10 18:52:50 +0530

    add files

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import streamlit as st
 import numpy as np
 from joblib import load
 from feature_pipeline import query_point_creator_tfidf
+import requests
 
 # Load model & vectorizer
 @st.cache_resource
@@ -18,15 +19,28 @@ st.title("Duplicate Question Detector")
 q1 = st.text_input("Enter Question 1")
 q2 = st.text_input("Enter Question 2")
 
+
+
 if st.button("Check"):
     if q1 and q2:
-        features = query_point_creator_tfidf(q1, q2, tfidf,STOP_WORDS)
-        result = model.predict(features)[0]
+        url = "https://duplicate-question-pairs-nlp-rdmv.onrender.com/predict"
 
-        
-        if result == 1:
-            st.success("✅ Duplicate Questions")
-        else:
-            st.error("❌ Not Duplicate Questions")
+        try:
+            with st.spinner("Checking..."):
+                response = requests.post(url, json={
+                    "q1": q1,
+                    "q2": q2
+                })
+
+            result = response.json()
+
+            if result["duplicate"] == 1:
+                st.success("✅ Duplicate Questions")
+            else:
+                st.error("❌ Not Duplicate Questions")
+
+        except Exception as e:
+            st.error("API Error: " + str(e))
+
     else:
         st.warning("Please enter both questions")
